13d8663c24a6086ce121c528473fc542117938f9
271915902 <271915902@qq.com>
2025-08-14 22:41:38 +0800

    变量默认值

---
+++ solidity-learning-lab/09_变量默认值.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract DefaultValues {
+    bool public flag; // false
+    uint256 public num; //0
+    address public addr; //0x000...;
+    bytes32 public b32; //0x000...;
+
+    // 定义的变量默认值
+}
