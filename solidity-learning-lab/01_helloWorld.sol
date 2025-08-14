e1c33951c71c8aa44c39bba7007951950fe3f9d1
271915902 <271915902@qq.com>
2025-08-14 17:43:09 +0800

    helloworld 合约

---
+++ solidity-learning-lab/01_helloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// 合约第一行要写一个版权声明协议 不写会报错
+pragma solidity ^0.8.30; // 第二行要写solidity的版本号
+
+// 定义合约语法 contract xxx {}
+contract HelloWorld {
+    string public greet = "Hello World!"; // 定义一个公共的字符串变量greet，并初始化为“Hello World!”
+}
