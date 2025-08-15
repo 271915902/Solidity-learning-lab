2722f68c7ac2accc5fb0f404d08c0f10576cc2a1
271915902 <271915902@qq.com>
2025-08-15 16:33:46 +0800

    可见范围

---
+++ solidity-learning-lab/26_可视范围.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+// private  私有 合约内部可以看到     子合约继承看不到
+// internal      合约内部可以看到     子合约继承可以看到
+
+// external 外部  合约内部看不到       合约外部才可以看到   子合约继承可以看到
+// public         在哪都可以看到
